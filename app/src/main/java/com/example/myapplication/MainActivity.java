157ef419a25a7feaffda5e612cf42d30231f0070
ilyesb36 <ilyesb36130@gmail.com>
2020-03-12 11:50:50 +0100

    Ajout Shop + Bouton pour aller menu principal

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -31,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(MainActivity.this, TicTacToeActivity.class);
         startActivity(intent);
     }
+
+
 }
 
 
