ef67101e7e30fda10f3b3e2362eae0d6b2b41604
Ruizheng <ruizhengxu@gmail.com>
2020-03-27 21:13:54 +0100

    voir les stats quand on donne à manger

--- app/src/main/java/com/example/myapplication/DinnerActivity.java
+++ app/src/main/java/com/example/myapplication/DinnerActivity.java
@@ -11,11 +11,20 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.akexorcist.roundcornerprogressbar.RoundCornerProgressBar;
+import com.example.myapplication.classes.Object;
 import com.example.myapplication.classes.ObjetctsBDD;
 
 import java.util.HashMap;
 
 public class DinnerActivity extends AppCompatActivity {
+
+    ObjetctsBDD objetctsBDD;
+
+    TextView level;
+    RoundCornerProgressBar expBar;
+    RoundCornerProgressBar healthBar;
+    RoundCornerProgressBar foodBar;
 
     TextView quantityFirst;
     TextView quantitySecond;
@@ -36,6 +45,20 @@ public class DinnerActivity extends AppCompatActivity {
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_dinner);
 
+        objetctsBDD = new ObjetctsBDD(this);
+        objetctsBDD.open();
+
+        level = findViewById(R.id.level);
+        level.bringToFront();
+        expBar = findViewById(R.id.expBar);
+        healthBar = findViewById(R.id.healthBar);
+        foodBar = findViewById(R.id.foodBar);
+
+        level.setText(String.valueOf(objetctsBDD.getQuantityWithTitle("Level")));
+        expBar.setProgress(objetctsBDD.getQuantityWithTitle("Experience"));
+        healthBar.setProgress(objetctsBDD.getQuantityWithTitle("Health"));
+        foodBar.setProgress(objetctsBDD.getQuantityWithTitle("Food"));
+
         list = new HashMap<>();
 
         list.put("Apple","apple");
@@ -44,15 +67,15 @@ public class DinnerActivity extends AppCompatActivity {
         list.put("Orange", "orange");
         list.put("Strawberry", "strawberry");
 
-        iFirst = (ImageView) findViewById(R.id.first);
-        iSecond = (ImageView) findViewById(R.id.second);
-        iThird = (ImageView) findViewById(R.id.third);
+        iFirst = findViewById(R.id.first);
+        iSecond = findViewById(R.id.second);
+        iThird = findViewById(R.id.third);
 
         updateList();
 
-        quantityFirst = (TextView) findViewById(R.id.quantityFirst);
-        quantitySecond = (TextView) findViewById(R.id.quantitySecond);
-        quantityThird = (TextView) findViewById(R.id.quantityThird);
+        quantityFirst = findViewById(R.id.quantityFirst);
+        quantitySecond = findViewById(R.id.quantitySecond);
+        quantityThird = findViewById(R.id.quantityThird);
 
         updateQuatity();
     }
@@ -112,9 +135,11 @@ public class DinnerActivity extends AppCompatActivity {
         objetctsBDD.open();
 
         String first = (String) list.keySet().toArray()[position];
-        if(objetctsBDD.getQuantityWithTitle(first)>0){
+        if (objetctsBDD.getQuantityWithTitle(first)>0){
             objetctsBDD.decrementerProduit(first,1);
-        }else {
+            increaseSatiety(10);
+            Toast.makeText(this, "La satiété a augmenté de 10", Toast.LENGTH_SHORT).show();
+        } else {
             Toast.makeText(this, "Vous n'avez pas assez de " + first + " ", Toast.LENGTH_SHORT).show();
         }
 
@@ -129,12 +154,14 @@ public class DinnerActivity extends AppCompatActivity {
         String second = "Banana";
         if (list.size()>position+1){
              second = (String) list.keySet().toArray()[position + 1];
-        }else {
+        } else {
              second = (String) list.keySet().toArray()[position - 3 + 1];
         }
-        if(objetctsBDD.getQuantityWithTitle(second)>0){
+        if (objetctsBDD.getQuantityWithTitle(second)>0){
             objetctsBDD.decrementerProduit(second,1);
-        }else {
+            increaseSatiety(10);
+            Toast.makeText(this, "La satiété a augmenté de 10", Toast.LENGTH_SHORT).show();
+        } else {
             Toast.makeText(this, "Vous n'avez pas assez de " + second+ " ", Toast.LENGTH_SHORT).show();
         }
         updateQuatity();
@@ -150,9 +177,12 @@ public class DinnerActivity extends AppCompatActivity {
         ObjetctsBDD objetctsBDD = new ObjetctsBDD(this);
         objetctsBDD.open();
 
-        if(objetctsBDD.getQuantityWithTitle(third)>0){
+        if (objetctsBDD.getQuantityWithTitle(third)>0){
             objetctsBDD.decrementerProduit(third,1);
-        }else {
+            increaseSatiety(20);
+            Toast.makeText(this, "La satiété a augmenté de 20", Toast.LENGTH_SHORT).show();
+            increaseExp(20);
+        } else {
             Toast.makeText(this, "Vous n'avez pas assez de " + third+ " ", Toast.LENGTH_SHORT).show();
         }
         updateQuatity();
@@ -173,5 +203,36 @@ public class DinnerActivity extends AppCompatActivity {
             updateList();
             updateQuatity();
         }
+    }
+
+    private void increaseSatiety(int value) {
+        ObjetctsBDD objetctsBDD  = new ObjetctsBDD(this);
+        objetctsBDD.open();
+        int satiety = objetctsBDD.getProduitWithTitre("Food").getQuantity() + value;
+        if (satiety > 200) {
+            satiety = 200;
+        }
+        foodBar.setProgress(satiety);
+        Object object = new Object("Food", satiety);
+        objetctsBDD.updateProduit("Food", object);
+    }
+
+    private void increaseExp(int value) {
+        ObjetctsBDD objetctsBDD = new ObjetctsBDD(this);
+        objetctsBDD.open();
+        int exp = objetctsBDD.getQuantityWithTitle("Experience") + value;
+        if (exp > 500) {
+            Object object = new Object("Experience", exp - 500);
+            objetctsBDD.updateProduit("Experience", object);
+            objetctsBDD.updateProduit("Level", new Object("Level", objetctsBDD.getQuantityWithTitle("Level") + 1));
+            expBar.setProgress(objetctsBDD.getQuantityWithTitle("Experience"));
+            level.setText(String.valueOf(objetctsBDD.getQuantityWithTitle("Level")));
+            Toast.makeText(this, "Bravo ! Votre Vimo a augmenté de niveau", Toast.LENGTH_LONG).show();
+        } else {
+            Object object = new Object("Experience", exp);
+            objetctsBDD.updateProduit("Experience", object);
+            expBar.setProgress(objetctsBDD.getQuantityWithTitle("Experience"));
+        }
+
     }
 }
